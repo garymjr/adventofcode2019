7db107c62aac87e27cb0dbe5214572cd2ece505c
Gary Murray <garymjr@gmail.com>
2019-12-04 06:32:49 -0700

    start working on day2 part2

--- day2/index.js
+++ day2/index.js
@@ -28,10 +28,34 @@ class Day2 {
     return intcode[0]
   }
 
-}
+  part2(input, expected, test = false) {
+    const intcode = input[0].split(',').map(n => Number(n))
 
-}
+    // apply 1202 program alarm
+    if (!test) {
+      intcode[1] = 12
+      intcode[2] = 2
+    }
 
+    for (let i = 0; i < intcode.length; i += 4) {
+      const [optCode, pos1, pos2, output] = intcode.slice(i, i + 4)
+
+      // stop if we receive a halt code or an invalid code
+      if (optCode === 99 || [1, 2, 99].indexOf(optCode) === -1) break;
+
+      let result = 0;
+      if (optCode === 1) {
+        result = intcode[pos1] + intcode[pos2]
+      } else if (optCode === 2) {
+        result = intcode[pos1] * intcode[pos2]
+      }
+
+      // console.log(result, expected)
+      if (result === expected) {
+        return 100 * pos1 + pos2
+      }
+    }
+  }
 }
 
 const day2 = new Day2()
@@ -41,3 +65,6 @@ test(day2.part1(['2,3,0,3,99'], true), 2)
 test(day2.part1(['2,4,4,5,99,0'], true), 2)
 test(day2.part1(['1,1,1,4,99,5,6,0,99'], true), 30)
 getResult(day2.part1(readInput('./day2/input.txt')))
+
+test(day2.part2(['1,12,2,0,1,1,1,1,1,1,1,1,2,99'], 4, true), 1202)
+getResult(day2.part2(readInput('./day2/input.txt'), 19690720))
