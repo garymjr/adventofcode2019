2b77fc0ff7290dd959cb8373a225f38b987cba3a
Gary Murray <garymjr@gmail.com>
2019-12-05 08:31:23 -0700

    day3 part1 passes, but very slow

--- day3/index.js
+++ day3/index.js
@@ -3,163 +3,54 @@ const test = require('../utils/test')
 const getResult = require('../utils/getResult')
 
 class Day3 {
-    part1(input) {
-        const wire1 = input[0].split(',').concat(input[1].split(','))
-        const wire2 = input[1].split(',')
+    constructor() {
+        this.x = { 'U': 0, 'D': 0, 'L': -1, 'R': 1 }
+        this.y = { 'U': 1, 'D': -1, 'L': 0, 'R': 0 }
+    }
 
+    part1(input) {
+        const wire1 = this.getPoints(input[0].split(','))
+        const wire2 = this.getPoints(input[1].split(','))
+        const both = this.findMatches(wire1, wire2)
+        return Math.min(...both)
+    }
+
+    getPoints(wire) {
         let x = 0
         let y = 0
-        let points = { 0: { 0: 1 } }
-        for (let i = 0; i < wire1.length; i++) {
-            const dir = wire1[i][0]
-            const value = Number(wire1[i].slice(1))
+        let points = wire.reduce((arr, cmd) => {
+            const dir = cmd[0]
+            const move = Number(cmd.slice(1))
+            for (let i = 0; i < move; i++) {
+                x += this.x[dir]
+                y += this.y[dir]
 
-            if (dir === 'R') {
-                const goal = x + value
-                while (x < goal) {
-                    x += 1
-                    if (points[x]) continue
-                    
-                    points = Object.assign({}, points, {
-                        [x]: { [y] : {} }
-                    })
-                }
+                const dups = arr.filter(([ax, ay]) => {
+                    return ax === x && ay === y
+                })
+
+                dups.length === 0 && arr.push([x, y])
             }
+            return arr
+        }, [])
+        return points
+    }
 
-            if (dir === 'L') {
-                const goal = x - value
-                while (x > goal) {
-                    x -= 1
-                    if (points[x]) continue
-                    
-                    points = Object.assign({}, points, {
-                        [x]: { [y]: {} }
-                    })
-                }
-            }
-
-            if (dir === 'U') {
-                const goal = y + value
-                while (y < goal) {
-                    y += 1
-                    if (points[x][y]) {
-                        points[x][y] += 1
-                    }
-                    
-                    points[x] = Object.assign({}, points[x], {
-                        [y]: 1
-                    })
-                }
-            }
-
-            if (dir === 'D') {
-                const goal = y - value
-                while (y > goal) {
-                    y -= 1
-                    if (points[x][y]) {
-                        points[x][y] -= 1
-                    }
-                    
-                    points[x] = Object.assign({}, points[x], {
-                        [y]: -1
-                    })
-                }
-            }
-        }
-        console.log(points)
-        // const wire1steps = wire1.reduce((steps, step) => {
-        //     const dir = step[0]
-        //     const value = Number(step.slice(1))
-            
-        //     let [x, y] = steps.slice(-1)[0]
-        //     if (dir === 'R') {
-        //         const goal = x + value
-        //         while (x < goal) {
-        //             x += 1
-        //             steps.push([x, y])
-        //         }
-        //     } else if (dir === 'L') {
-        //         const goal = x - value
-        //         while (x > goal) {
-        //             x -= 1
-        //             steps.push([x, y])
-        //         }
-        //     } else if (dir === 'U') {
-        //         const goal = y + value
-        //         while (y < goal) {
-        //             y += 1
-        //             steps.push([x, y])
-        //         }
-        //         y += value
-        //     } else if (dir === 'D') {
-        //         const goal = y - value
-        //         while (y > goal) {
-        //             y -= 1
-        //             steps.push([x, y])
-        //         }
-        //     }
-
-        //     return steps
-        // }, [[0, 0]])
-
-        // const wire2steps = wire2.reduce((steps, step) => {
-        //     const dir = step[0]
-        //     const value = Number(step.slice(1))
-            
-        //     let [x, y] = steps.slice(-1)[0]
-        //     if (dir === 'R') {
-        //         const goal = x + value
-        //         while (x <= goal) {
-        //             x += 1
-        //             steps.push([x, y])
-        //         }
-        //     } else if (dir === 'L') {
-        //         const goal = x - value
-        //         while (x >= goal) {
-        //             x -= 1
-        //             steps.push([x, y])
-        //         }
-        //     } else if (dir === 'U') {
-        //         const goal = y + value
-        //         while (y <= goal) {
-        //             y += 1
-        //             steps.push([x, y])
-        //         }
-        //         y += value
-        //     } else if (dir === 'D') {
-        //         const goal = y - value
-        //         while (y >= goal) {
-        //             y -= 1
-        //             steps.push([x, y])
-        //         }
-        //     }
-
-        //     return steps
-        // }, [[0, 0]])
-
-        // let distance;
-        // for (let i = 0; i < wire1steps.length; i++) {
-        //     const [ix, iy] = wire1steps[i]
-        //     // if (ix === 0 || iy === 0) continue
-
-        //     for (let j = 0; j < wire2steps.length; j++) {
-        //         const [jx, jy] = wire2steps[j]
-        //         if (jx === 0 || jy === 0) continue
-
-        //         if (ix === jx && iy === jy) {
-        //             if (!distance || distance >= (Math.abs(jx) + Math.abs(jy))) {
-        //                 distance = Math.abs(jx) + Math.abs(jy)
-        //             }
-        //         }
-        //     }
-        // }
-        return 0
+    findMatches(wire1, wire2) {
+        const matches = []
+        wire1.forEach(wire => {
+            const [match] = wire2.filter(([x, y]) => {
+                return wire[0] === x && wire[1] === y
+            })
+            match && matches.push(Math.abs(match[0]) + Math.abs(match[1]))
+        })
+        return matches
     }
 }
 
 const day3 = new Day3()
 
 test(day3.part1(['R8,U5,L5,D3', 'U7,R6,D4,L4']), 6)
-// test(day3.part1(['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83']), 159)
-// test(day3.part1(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']), 135)
-// getResult(day3.part1(readInput('./day3/input.txt')))
+test(day3.part1(['R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83']), 159)
+test(day3.part1(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']), 135)
+getResult(day3.part1(readInput('./day3/input.txt')))
